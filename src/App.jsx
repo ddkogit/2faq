5ea0a8af6f2e49c9dfc05ad52e1f2caace5bb7d6
ddkogit <git config --global user.email>
2024-01-30 21:12:37 +0545

    first making

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Block from './components/Block'
+
+function App() {
+
+  return (
+    <>
+<div className='container'>
+
+    <h2>Frequently Asked Questions</h2>
+    <Block />
+</div>
+
+
+    </>
+  )
+}
+
+export default App
