a0bd787d143a70e6c5b1a11b3052693cb64e8664
ddkogit <git config --global user.email>
2024-01-30 22:09:25 +0545

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 
+import { useState } from 'react'
 import './App.css'
 import Block from './components/Block'
 
+const arr=[
+  {
+    "question": "q1 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+    "answer": "a1 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+  },
+  {
+    "question": "q2 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+    "answer": "a2 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+  },
+  {
+    "question": "q3 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+    "answer": "a3 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+  },
+  {
+    "question": "q4 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+    "answer": "a4 uisquam ipsam dolorum libero eum nisi amet ea, totam minus atsoluta ",
+  },
+]
+
 function App() {
+
+  
 
   return (
     <>
 <div className='container'>
 
     <h2>Frequently Asked Questions</h2>
-    <Block />
+
+    {
+      arr.map((item,i)=>(
+
+        <Block key={i} question={item.question}
+         answer={item.answer}
+          />
+      ))
+    }
 </div>
 
 
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 
-function Block() {
+function Block({question,answer}) {
+
+    
+  const [hidden,setHidden] = useState(false);
+
+  
+  const handleClick =(e)=>{
+
+
+        setHidden(!hidden);
+        
+    
+  }
+
+
   return (
-    <div>Block</div>
+    <div className="block">
+        <div className="question" onClick={(e)=>handleClick(e)}>
+            {question}
+            <button className="button">
+                {hidden?"+":"-"}
+            </button>
+        </div>
+
+{
+    hidden?<></>:
+
+        <div className="answer">
+            {answer}
+        </div>
+}
+    </div>
   )
 }
 
